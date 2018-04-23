14686c990e47481774b1ef6b1df8bc4495426071
David Lee <daviddwlee84@gmail.com>
2018-04-23 23:16:08 +0800

    modify to express.Router

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const app = express()
+const indexRouter = require('./routes/index')
+const userRouter = require('./routes/users')
 
-app.get('/', function (req, res) {
-  res.send('hello, express')
-})
-
-app.get('/users/:name', function (req, res) {
-  res.send('hello, ' + req.params.name)
-})
+app.use('/', indexRouter)
+app.use('/users', userRouter)
 
 app.listen(3000)
 
