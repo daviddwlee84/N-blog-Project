a601130e7f99e31ab00755e4ba2b87b75820f311
David Lee <daviddwlee84@gmail.com>
2018-04-23 23:09:33 +0800

    add another router in index.js

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ app.get('/', function (req, res) {
   res.send('hello, express')
 })
 
+app.get('/users/:name', function (req, res) {
+  res.send('hello, ' + req.params.name)
+})
+
 app.listen(3000)
 
