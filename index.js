88cff08f93c5fb9c604f30c0646888aa8648f0bb
David Lee <daviddwlee84@gmail.com>
2018-04-25 14:22:11 +0800

    Delete all practice purpose file to empty project

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-const indexRouter = require('./routes/index')
-const userRouter = require('./routes/users')
-const ejstestRouter = require('./routes/ejstest')
 
 app.set('views', path.join(__dirname, 'views')) // Setting directory of default template files.
 app.set('view engine', 'ejs') // Setting template engine as ejs.
-
-app.use('/', indexRouter)
-app.use('/users', userRouter)
-app.use('/ejstest', ejstestRouter)
 
 app.listen(3000)
 
