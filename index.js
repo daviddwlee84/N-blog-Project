84ec8e99d7175476d24e6e0e11b06dc94fe9b182
David Lee <daviddwlee84@gmail.com>
2018-04-25 16:03:35 +0800

    Import express-session

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const app = express()
 app.set('views', path.join(__dirname, 'views')) // Setting directory of default template files.
 app.set('view engine', 'ejs') // Setting template engine as ejs.
 
-app.listen(3000)
+app.use(session(options)) // Import express-session to support session
 
