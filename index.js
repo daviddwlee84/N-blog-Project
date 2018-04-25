5cacec6b41a5584dde81a89d8a37d8c01dd0daf4
David Lee <daviddwlee84@gmail.com>
2018-04-25 16:34:07 +0800

    Modify main program

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
+// The order of loading middleware is very important
 const path = require('path')
 const express = require('express')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('connect-flash')
+const config = require('config-lite')(__dirname)
+const routes = require('./routes')
+const pkg = require('./package')
+
 const app = express()
 
-app.set('views', path.join(__dirname, 'views')) // Setting directory of default template files.
-app.set('view engine', 'ejs') // Setting template engine as ejs.
+// Setting directory of default template files.
+app.set('views', path.join(__dirname, 'views'))
+// Setting template engine as ejs.
+app.set('view engine', 'ejs')
 
-app.use(session(options)) // Import express-session to support session
+// Setting Static file directory
+app.use(express.static(path.join(__dirname, 'public')))
+// Import express-session to support session
+app.use(session({
+  name: config.session.key, // Setting session id store in cookie
+  secret: config.session.secret, // Calculate hash value by setting secret and then store in cookie, produce signedCookie to prevent altering
+  resave: true, // Force update session
+  saveUninitialized: false, // Setting false. Force to create a session even though user doesn't login
+  cookie: {
+    maxAge: config.session.maxAge// Overdue time. When it reach expiration, automatically delete session id stored in cookie
+  },
+  store: new MongoStore({// Store session to mongodb
+    url: config.mongodb// Mongodb address
+  })
+}))
+// flash middleware. Used to show notification
+app.use(flash())
 
+// Router
+routes(app)
+
+// Lissening Port and Start Program
+app.listen(config.port, function () {
+  console.log(`${pkg.name} listening on port ${config.port}`)
+})
