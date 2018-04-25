06da614ffb2aef781a3b27600a228c2a2f70c743
David Lee <daviddwlee84@gmail.com>
2018-04-25 10:28:26 +0800

    ejstest. Another example for ejs rendering

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const express = require('express')
 const app = express()
 const indexRouter = require('./routes/index')
 const userRouter = require('./routes/users')
+const ejstestRouter = require('./routes/ejstest')
 
 app.set('views', path.join(__dirname, 'views')) // Setting directory of default template files.
 app.set('view engine', 'ejs') // Setting template engine as ejs.
 
 app.use('/', indexRouter)
 app.use('/users', userRouter)
+app.use('/ejstest', ejstestRouter)
 
 app.listen(3000)
 
